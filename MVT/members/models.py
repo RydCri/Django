9e38bdc529c088c5e81816f1dbf8cc230b0df646
RydCri <107228115+RydCri@users.noreply.github.com>
2023-10-20 11:45:12 -0500

    display first and lastname as model object

--- MVT/members/models.py
+++ MVT/members/models.py
@@ -8,3 +8,6 @@ class Member(models.Model):
     lastname = models.CharField(max_length=255)
     gender = models.CharField(max_length=3, null=True)
     joined_date = models.DateField(null=True)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
