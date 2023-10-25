2852b7062f247c5700d82b704bcb21e5dd4e8a25
RydCri <107228115+RydCri@users.noreply.github.com>
2023-10-25 01:15:08 -0500

    /createPoll /createChoice

--- MVT/polls/urls.py
+++ MVT/polls/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('polls/', views.polls, name='polls'),  # views/polls
-    path("polls/create", views.new_poll, name="new_poll"),
+    path("polls/createPoll", views.new_poll, name="new_poll"),
+    path("polls/createChoice", views.new_choice, name="new_choice"),
     path("polls/<int:question_id>/", views.detail, name="detail"),
     path("polls/<int:question_id>/results/", views.results, name="results"),
     path("polls/<int:question_id>/vote/", views.vote, name="vote"),
