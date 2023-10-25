22cac8f7ba6c92235903730a63a6494f50904731
RydCri <107228115+RydCri@users.noreply.github.com>
2023-10-25 01:14:33 -0500

    this needs some work

--- MVT/polls/forms.py
+++ MVT/polls/forms.py
@@ -1,30 +1,49 @@
 from attr.filters import exclude
 from django.forms import ModelForm
+from django.utils import timezone, dateformat
+
 from .models import Question, Choice
+from .models import models
 
 
 class QuestionForm(ModelForm):
     class Meta:
         model = Question
-        exclude = ['pub_date']
+        fields = "__all__"
         labels = {
             "question_text": "Topic",
         }
+
     help_texts = {
         "question_text": "Provide a topic for your Poll",
     }
+
+    def __init__(self, *args, **kwargs):
+        formatted_date = dateformat.format(timezone.now(), 'Y-m-d H:i:s')
+        last_question = Question.objects.order_by("-pub_date")[0]
+        super(QuestionForm, self).__init__(*args, **kwargs)
+        self.fields['pub_date'].initial = formatted_date
+        # self.fields['id'].initial = last_question
 
 
 class ChoiceForm(ModelForm):
     class Meta:
         model = Choice
-        exclude = ["votes", "question"]
+        fields = '__all__'
         labels = {
             "choice_text1": "Choices",
             "choice_text2": "Choices",
             "choice_text3": "Choices",
             "choice_text4": "Choices",
         }
+
     help_texts = {
         "question_text": "Please provide at least two Choices for your Poll",
     }
+
+
+def __init__(self, *args, **kwargs):
+    last_question = Question.objects.last()
+    super(QuestionForm, self).__init__(*args, **kwargs)
+    self.fields['question_id'].initial = last_question.get(id())
+    self.fields['votes'].initial = 0
