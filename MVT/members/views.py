93b232740f04a56eb762fc0deda33d3f37da5e4a
RydCri <107228115+RydCri@users.noreply.github.com>
2023-10-27 13:27:08 -0500

    Replaced member model with User model in def index, details, allmembers

--- MVT/members/views.py
+++ MVT/members/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
@@ -5,7 +6,7 @@ from .models import Member
 
 
 def members(request):
-    mymembers = Member.objects.all().values()
+    mymembers = User.objects.all().values()
     template = loader.get_template('allmembers.html')
     context = {
         'mymembers': mymembers,
@@ -15,11 +16,17 @@ def members(request):
 
 def index(request):
     template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    user = request.user
+    if request.user.is_authenticated:
+        print(user.username + ' is logged in')
+    context = {
+        'user': user
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def details(request, id):
-    mymember = Member.objects.get(id=id)
+    mymember = User.objects.get(id=id)
     template = loader.get_template('details.html')
     context = {
         'mymember': mymember,
