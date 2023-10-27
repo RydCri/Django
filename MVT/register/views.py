5c28ec12f65cdae80f79e55155670bb3e6f585e5
RydCri <107228115+RydCri@users.noreply.github.com>
2023-10-27 13:25:55 -0500

    register login logout views

--- MVT/register/views.py
+++ MVT/register/views.py
@@ -1,5 +1,9 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib.auth import logout, login, authenticate
 
 
 def register(response):
@@ -13,3 +17,28 @@ def register(response):
         form = RegisterForm()
 
     return render(response, "register/register.html", {"form": form})
+
+
+def custom_login(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+    if request.user.is_authenticated:
+        return redirect('/')
+
+    form = AuthenticationForm()
+
+    return render(
+        request=request,
+        template_name="registration/login.html",
+        context={'form': form}
+    )
+
+
+@login_required
+def custom_logout(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("/")
