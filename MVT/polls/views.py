eff382c0caada87a6843b5405a148f27f1c67f6c
RydCri <107228115+RydCri@users.noreply.github.com>
2023-10-28 23:48:50 -0500

    rf

--- MVT/polls/views.py
+++ MVT/polls/views.py
@@ -1,11 +1,8 @@
-from django.conf import settings
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.http import Http404
-from django.utils import timezone
 
 from .forms import QuestionForm, ChoiceForm
 from .models import Choice
